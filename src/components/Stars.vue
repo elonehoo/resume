bcceda20c9ff948f30ed6f5aeecc674a1bbc7014
xiaoxunyao <huchengyea@163.com>
2022-04-22 23:37:25 +0800

    chore: add `stars`

---
+++ src/components/Stars.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import {ref} from 'vue'
+  import axios from 'axios'
+  const date = ref()
+  const props = withDefaults(defineProps<{
+    url:string
+  }>(),{})
+  axios.get(props.url).then(res=>{
+    date.value = res.data.stargazers_count
+  })
+</script>
+
+<template>
+  <div>
+    star: <span>{{date}}</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
