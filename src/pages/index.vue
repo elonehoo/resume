e369efc816eac14378b4ead3386733076bc36dda
xiaoxunyao <huchengyea@163.com>
2022-04-22 19:38:22 +0800

    fix: add css class `container`

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,24 @@
 </script>
 
 <template>
-  <div>
-    
+  <div class="container">
+111
   </div>
 </template>
+
+<style scoped>
+.container{
+  display: grid;
+  grid-template-columns: auto 15fr 50fr auto;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+@media (min-width: 768px){
+  .container{
+    width: 750px;
+  }
+}
+</style>
